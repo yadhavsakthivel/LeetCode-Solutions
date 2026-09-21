c95f6d0eca435d8cd63cdae05252396e45420cb0
Gnana siva yadhav s <yadhavsakthivel24@gmail.com>
2026-09-21 16:05:05 +0530

    Time: 14 ms (25.60%) | Memory: 46.8 MB (61.60%) - LeetSync

---
+++ 1168-duplicate-zeros/duplicate-zeros.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        int n=arr.length;
+        for(int i=0; i<n-1; i++)
+        {
+            if(arr[i]==0)
+            {
+                for(int j=n-1; j>i; j--)
+                {
+                    arr[j]=arr[j-1];
+                }
+                i++;
+            }
+        }
+    }
+}
